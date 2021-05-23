0312be0a5a165214ca9dc616dbfb96e96e9d0da5
Thone Cardoso de Araujo <thonecardoso@gmail.com>
2021-05-23 10:08:27 -0300

    Injeção de Dependência

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -10,14 +10,15 @@ import { CursosService } from './cursos.service';
 export class CursosComponent implements OnInit {
 
   cursos: string[] = [];
-  cursoService!: CursosService;
+  // cursoService!: CursosService;
 
-  constructor() {
-    this.cursoService = new CursosService();
+  constructor(private cursoServices: CursosService) {
+    // this.cursoService = new CursosService();
+    // this.cursoService = cursoServices;
   }
 
   ngOnInit(): void {
-    this.cursos = this.cursoService.getCursos();
+    this.cursos = this.cursoServices.getCursos();
   }
 
 }
