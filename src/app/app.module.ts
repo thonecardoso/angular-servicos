9f995526520fbc4cb481ef9262193068baa0cd3d
Thone Cardoso de Araujo <thonecardoso@gmail.com>
2021-05-23 14:31:36 -0300

    Injetando um serviço em outro serviço

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import {CursosService} from './cursos/cursos.service';
-import { CriarCursoModule } from './criar-curso/criar-curso.module';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {CriarCursoModule} from './criar-curso/criar-curso.module';
 import {CursosModule} from './cursos/cursos.module';
+import {LogService} from './shared/log.service';
 
 @NgModule({
   declarations: [
@@ -17,7 +17,7 @@ import {CursosModule} from './cursos/cursos.module';
     CriarCursoModule,
     CursosModule
   ],
-  // providers: [CursosService],
+  providers: [LogService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/cursos/cursos.service.ts
+++ src/app/cursos/cursos.service.ts
@@ -1,9 +1,10 @@
 import {Injectable, EventEmitter} from '@angular/core';
+import {LogService} from '../shared/log.service';
 
 @Injectable()
 export class CursosService {
 
-  constructor() {
+  constructor(private logService: LogService) {
     console.log('CursosService');
   }
 
@@ -14,10 +15,12 @@ export class CursosService {
   private cursos = ['Angular 2', 'Java', 'PHP'];
 
   getCursos(): Array<string> {
+    this.logService.consoleLog('Obtendo Lista de cursos');
     return this.cursos;
   }
 
   addCurso(curso: string): void {
+    this.logService.consoleLog(`Criando um novo curso: ${curso}` );
     this.cursos.push(curso);
     this.emitirCursoCriado.emit(curso);
     CursosService.criouNovoCurso.emit(curso);
